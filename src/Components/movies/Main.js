78d8a7bcb349d952d9f4b2e3798ec23ed8a1a127
ajdinl <ajdinl@live.com>
2022-01-18 17:29:26 +0100

    css hover edited, modal added and little bit configured

--- src/Components/movies/Main.js
+++ src/Components/movies/Main.js
@@ -1,11 +1,14 @@
+import { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { FETCH_MOVIES } from '../../features/moviesSlice'
 import { TOP_MOVIE_API_URL } from '../../Api'
 import useFetch from '../../hooks/useFetch'
 
 const Main = () => {
+  const [showModal, setShowModal] = useState(false)
   useFetch(TOP_MOVIE_API_URL, FETCH_MOVIES)
   const moviesData = useSelector((state) => state.movies)
+
   return (
     <div>
       <div className='trending-title'>TRENDING</div>
@@ -22,10 +25,6 @@ const Main = () => {
         ) : (
           moviesData.data.map((result, index) => (
             <div key={index}>
-              <img
-                src={`https://image.tmdb.org/t/p/w400/${result.poster_path}`}
-                alt='movie'
-              />
               <div className='trending-movie-details'>
                 <div className='trending-movie-title'>{result.title}</div>
                 <div className='trending-movie-description'>
@@ -33,13 +32,48 @@ const Main = () => {
                 </div>
                 <button
                   onClick={() => {
-                    alert('hello')
+                    setShowModal(true)
                   }}
                   className='reserve-now-btn'
                 >
                   Reserve now
                 </button>
+                {showModal ? (
+                  <div className='modal-container'>
+                    <div className='modal-content'>
+                      <div className='modal-reservation-image'>
+                        <h3>RESERVE YOUR PLACE</h3>
+                        <img
+                          src={`https://image.tmdb.org/t/p/w400/${result.poster_path}`}
+                          alt='movie'
+                        />
+                      </div>
+                      <div className='modal-reservation-details'>
+                        <div>
+                          <h4>NAME</h4>
+                          <p>{result.title}</p>
+                        </div>
+                        <div>
+                          <h4>Amount</h4> 1 -- 3 ------------ 20
+                        </div>
+                        <div>
+                          <h4>DATE</h4> 24 - 12 - 2021
+                        </div>
+                        <div>
+                          <h4>TIME</h4> 20:30
+                        </div>
+                        <button onClick={() => setShowModal(false)}>
+                          Cancel
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                ) : null}
               </div>
+              <img
+                src={`https://image.tmdb.org/t/p/w400/${result.poster_path}`}
+                alt='movie'
+              />
             </div>
           ))
         )}
--- src/Components/movies/MoviesDetails.js
+++ src/Components/movies/MoviesDetails.js
@@ -1,9 +1,11 @@
+import { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { FETCH_MOVIES_DETAILS } from '../../features/moviesSlice'
 import { UPCOMING_MOVIE_API_URL } from '../../Api'
 import useFetch from '../../hooks/useFetch'
 
 const MoviesDetails = () => {
+  const [showModal, setShowModal] = useState(false)
   useFetch(UPCOMING_MOVIE_API_URL, FETCH_MOVIES_DETAILS)
   const moviesData = useSelector((state) => state.movies)
 
@@ -20,29 +22,57 @@ const MoviesDetails = () => {
         </div>
       ) : (
         moviesData.details.map((result, index) => (
-          <>
-            <div className='second-movies-details'>
-              <div key={index}>
-                <img
-                  src={`https://image.tmdb.org/t/p/w400/${result.poster_path}`}
-                  alt='movie'
-                />
-
-                <h2> {result.title} </h2>
-                <p>{result.overview}</p>
-                <div className='second-movie-details-hidden'>
-                  <button
-                    onClick={() => {
-                      alert('hello')
-                    }}
-                    className='reserve-now-btn reserve-now-btn-second'
-                  >
-                    Reserve now
-                  </button>
-                </div>
+          <div className='second-movies-details'>
+            <div key={index}>
+              <div className='second-movie-details-hidden'>
+                <button
+                  onClick={() => {
+                    setShowModal(true)
+                  }}
+                  className='reserve-now-btn reserve-now-btn-second'
+                >
+                  Reserve now
+                </button>
+                {showModal ? (
+                  <div className='modal-container'>
+                    <div className='modal-content'>
+                      <div className='modal-reservation-image'>
+                        <h3>RESERVE YOUR PLACE</h3>
+                        <img
+                          src={`https://image.tmdb.org/t/p/w400/${result.poster_path}`}
+                          alt='movie'
+                        />
+                      </div>
+                      <div className='modal-reservation-details'>
+                        <div>
+                          <h4>NAME</h4>
+                          <p>{result.title}</p>
+                        </div>
+                        <div>
+                          <h4>Amount</h4> 1 -- 3 ------------ 20
+                        </div>
+                        <div>
+                          <h4>DATE</h4> 24 - 12 - 2021
+                        </div>
+                        <div>
+                          <h4>TIME</h4> 20:30
+                        </div>
+                        <button onClick={() => setShowModal(false)}>
+                          Cancel
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                ) : null}
               </div>
+              <img
+                src={`https://image.tmdb.org/t/p/w400/${result.poster_path}`}
+                alt='movie'
+              />
+              <h2> {result.title} </h2>
+              <p>{result.overview}</p>
             </div>
-          </>
+          </div>
         ))
       )}
     </div>
